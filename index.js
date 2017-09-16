5c83f25aeb8175eddbf8176a419b19edb3965c63
Stephen Chung <spchung@andrew.cmu.edu>
2017-09-16 12:01:54 -0400

    create initial landing page

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var path = require('path')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, function() {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});

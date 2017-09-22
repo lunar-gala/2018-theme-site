a5f864ef4d93e4d3e54612efbabd1488d858de42
Stephen Chung <spchung@andrew.cmu.edu>
2017-09-22 18:07:42 -0400

    intro code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express')
 var path = require('path')
 var app = express()
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 })

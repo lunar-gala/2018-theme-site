b5236985bf71c109643f28256e5df8b21637324a
Stephen Chung <spchung@andrew.cmu.edu>
2017-11-03 13:40:39 -0400

    add html folder

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+app.get('/about', function (req, res) {
+
+});
+
+app.get('/lines', function (req, res) {
+
+})
+
+app.get('/people', function (req, res) {
+
+})
+
 app.get('*', function (req, res) {
   res.status(404).send("Status code 404 - The URL you are looking for does not exist");
 });

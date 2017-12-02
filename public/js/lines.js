35886d6902120db4059bad2d421bf9fb9495e60b
Stephen Chung <spchung@andrew.cmu.edu>
2017-12-02 15:44:38 -0500

    lines

--- public/js/lines.js
+++ public/js/lines.js
@@ -1,6 +1,21 @@
+var TOPSELECTOR = "#3_2"
+var BOTTOMSELECTOR = "#7_0"
+var LEFTSELECTOR = "#3_0"
+var RIGHTSELECTOR = "#3_6"
+var MIDDLESELECTOR = "#5_3"
+
 $(window).ready(function () {
     $(window).scroll(function () {
-        alert("scrolling")
+
+        var blocksToChange = {}
+        blocksToChange[TOPSELECTOR] = "top"
+        blocksToChange[BOTTOMSELECTOR] = "bottom"
+        blocksToChange[LEFTSELECTOR] = "left"
+        blocksToChange[RIGHTSELECTOR] = "right"
+        blocksToChange[MIDDLESELECTOR] = "middle"
+
+        changeContent(blocksToChange)
+        console.log("scrolled")
     })
 
     animateBlock("#0_0", 0,1);
@@ -29,12 +44,12 @@ $(window).ready(function () {
         .click(function () {
             $(".block").toggleClass('clicked')
         })
-        .html("<div style='margin-left: 50%'>amzu-amzu</div>")
+        .html("<div class='linesTop' style='margin-left: 50%'>amzu-amzu</div>")
 
     // 2228
     animateBlock("#3_0", 2,0, true);
     $(".mainGrid #3_0 .inner")
-        .html("<div style='margin-top: 65%; margin-left: 66%'>2288</div>")
+        .html("<div class='linesLeft' style='margin-top: 65%; margin-left: 66%'>2288</div>")
         .addClass("contentLink aboutImg1")
         .click(function () {
             $(".block").toggleClass('clicked')
@@ -43,7 +58,7 @@ $(window).ready(function () {
     // chinoiseries
     animateBlock("#5_3", 0,2, true);
     $(".mainGrid #5_3 .inner")
-        .html("<div style='margin-left: -100%'>chinoiseries</div>")
+        .html("<div class='linesMiddle' style='margin-left: -100%'>chinoiseries</div>")
         .addClass("contentLink aboutImg1")
         .click(function () {
             $(".block").toggleClass('clicked')
@@ -52,7 +67,7 @@ $(window).ready(function () {
     // chroma
     animateBlock("#3_6", 2,0, true);
     $(".mainGrid #3_6 .inner")
-        .html("<div style='margin-top: 65%; margin-left: 60%'>chroma</div>")
+        .html("<div class='linesRight' ='margin-top: 65%; margin-left: 60%'>chroma</div>")
         .addClass("contentLink aboutImg1")
         .click(function () {
             $(".block").toggleClass('clicked')
@@ -61,7 +76,7 @@ $(window).ready(function () {
     // descent
     animateBlock("#7_0", 0,2, true);
     $(".mainGrid #7_0 .inner")
-        .html("<div style='margin-left: 85%'>descent</div>")
+        .html("<div class='linesBottom' style='margin-left: 85%'>descent</div>")
         .addClass("contentLink aboutImg1")
         .click(function () {
             $(".block").toggleClass('clicked')
@@ -75,32 +90,37 @@ function changeLeftBlock(text, imgClass) {
 }
 
 function changeContent(blockSizings) {
-    // amzu-amzu
-    $(".mainGrid #3_2 .inner")
-        .text("amzu-amzu")
-        .removeClass("aboutImg1")
+    $.each(blockSizings, function (blockSelector, val) {
+        console.log(blockSelector)
+        $(blockSelector + " .inner").text(val)
+    })
 
-    // 2228
-    $(".mainGrid #3_0 .inner")
-        .text("2288")
-        .removeClass("aboutImg1")
-        .click(function () {
-            $(".block").toggleClass('clicked')
-        });
+    // // amzu-amzu
+    // $(".mainGrid #3_2 .inner")
+    //     .text("amzu-amzu")
+    //     .removeClass("aboutImg1")
 
-    // chinoiseries
-    $(".mainGrid #5_3 .inner")
-        .text("chinoiseries")
-        .removeClass("aboutImg1")
+    // // 2228
+    // $(".mainGrid #3_0 .inner")
+    //     .text("2288")
+    //     .removeClass("aboutImg1")
+    //     .click(function () {
+    //         $(".block").toggleClass('clicked')
+    //     });
 
-    // chroma
-    $(".mainGrid #3_6 .inner")
-        .text("chroma")
-        .removeClass("aboutImg1")
+    // // chinoiseries
+    // $(".mainGrid #5_3 .inner")
+    //     .text("chinoiseries")
+    //     .removeClass("aboutImg1")
 
-    // descent
-    $(".mainGrid #7_0 .inner")
-        .text("descent")
-        .removeClass("aboutImg1")
+    // // chroma
+    // $(".mainGrid #3_6 .inner")
+    //     .text("chroma")
+    //     .removeClass("aboutImg1")
+
+    // // descent
+    // $(".mainGrid #7_0 .inner")
+    //     .text("descent")
+    //     .removeClass("aboutImg1")
 }
 

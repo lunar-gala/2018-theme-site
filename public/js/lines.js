962fb0cf327dff3c55add14ee00ede7865696aa9
Stephen Chung <spchung@andrew.cmu.edu>
2018-01-30 20:29:56 -0500

    animate mobile

--- public/js/lines.js
+++ public/js/lines.js
@@ -32,7 +32,7 @@ for (var i = 0; i < linedata.length; i++) {
     var line = linedata[i];
     LINES.push(new Line(line.title, line.designers, line.description));
 }
-console.log(LINES.length/5)
+
 function init_lines_mobile() {
     LEFTBLOCK = ".mainGrid #0_0 .inner"
     TOPBLOCK = ".mainGrid #0_1 .inner" 
@@ -46,8 +46,29 @@ function init_lines_mobile() {
     animateBlock("#0_1", 0,2, true); // TOP BLOCK
     animateBlock("#2_1", 0,1, true); // MIDDLE BLOCK
 
+    animateBlock("#5_0", 0,1, true); // LEFT BLOCK
+    animateBlock("#5_1", 0,2, true); // TOP BLOCK
+    animateBlock("#7_1", 0,1, true); // MIDDLE BLOCK
+
+    animateBlock("#10_0", 0,1, true); // LEFT BLOCK
+    animateBlock("#10_1", 0,2, true); // TOP BLOCK
+    animateBlock("#12_1", 0,1, true); // MIDDLE BLOCK
+
+    animateBlock("#15_0", 0,1, true); // LEFT BLOCK
+    animateBlock("#15_1", 0,2, true); // TOP BLOCK
+    animateBlock("#17_1", 0,1, true); // MIDDLE BLOCK
+
+    animateBlock("#20_0", 0,1, true); // LEFT BLOCK
+    animateBlock("#20_1", 0,2, true); // TOP BLOCK
+    animateBlock("#22_1", 0,1, true); // MIDDLE BLOCK
+
+    animateBlock("#25_0", 0,1, true); // LEFT BLOCK
+    animateBlock("#25_1", 0,2, true); // TOP BLOCK
+    animateBlock("#27_1", 0,1, true); // MIDDLE BLOCK
+
+
     populateLinesBlocks();
-    setLines(0)
+
     $("body").off("click").click(clickLinesPicture);
 }
 
@@ -97,16 +118,11 @@ function init_lines() {
     animateBlock("#15_0",2,0,true); // left
     animateBlock("#17_3",0,2,true); // middle
     animateBlock("#15_6",2,0,true); // right
-    animateBlock("#19_0",0,2,true); // bottom
 
     populateLinesBlocks()
 
     $("body").off("click").click(clickLinesPicture);
 }
-
-// TODO: play around with these numbers
-var FADEIN_DURATION = 300;
-var FADEOUT_DURATION = 300;
 
 function setLineBlock(selector, line) {
     var titleSelector = selector + " .title"
@@ -121,18 +137,10 @@ function setLineBlock(selector, line) {
         return;
     }
 
-    $(titleSelector).fadeOut(FADEOUT_DURATION,function() { $(this).text(line.title).fadeIn(FADEIN_DURATION)});
+    $(titleSelector).text(line.title);
     $(designerSelector).text(line.designers);
     $(descriptionSelector).text(line.description);
     $(selector).attr("id", line.imageId());
-
-    setTimeout(function(){
-        var elemwidth = $(selector).find(".title")[0].getBoundingClientRect().width/2;
-        var translatestring = "translateX(-"+elemwidth+"px)"
-        $(selector + " .content").css({
-            transform: translatestring
-        })
-    },320);
 }
 
 function clickLinesPicture(e) {
@@ -167,19 +175,28 @@ function populateLinesBlocks() {
 
     for (var lineset=0; lineset < totalSets; lineset++) {
         selectorblocks.forEach(function(selector, i) {
+            if ((i + lineset * LINESETSIZE) > LINES.length - 1) {
+                return;
+            }
             var id = selector.split("#")[1]
             var baserow = id.split("_")[0];
             var basecol = id.split("_")[1];
             var actualrow = parseInt(baserow) + lineset*5;
             var actualselector = "#" + actualrow + "_" + basecol;
 
-            console.log(actualselector)
             $(actualselector)
                 .html("<div class='content'><span class='title'></span><p class='designers'></p><p class='description'></p></div>")
                 .addClass("linesBlock lineBlockPicMuted " + selectornames[i])
+
             var line = LINES[lineset*LINESETSIZE + i];
             setLineBlock(actualselector, line)
-            console.log(line)
+            setTimeout(function(){centerContent(actualselector,line)},330);
         })
     }
+}
+
+function centerContent(selector, line) {
+    var elemwidth = $(selector).find(".title")[0].getBoundingClientRect().width/2;
+    var translatestring = "translateX(-"+elemwidth+"px)"
+    $(selector + " .content").css({transform: translatestring })
 }

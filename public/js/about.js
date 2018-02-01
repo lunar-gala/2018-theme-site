7f2ae919a1b04affa0d76936ab9e6af782c9acee
Deniz A. Sokullu <deniz.sokullu@ibm.com>
2018-02-01 12:32:12 -0500

    added X button to intro

--- public/js/about.js
+++ public/js/about.js
@@ -4,7 +4,7 @@ function init_about(){
   animateBlock("#title_1_1",0,1);
   animateBlock("#title_0_6",0,1);
 
-  $("#title_0_6 .inner").text("Humans").addClass("topLink");
+  $("#title_0_6 .inner").text("Members").addClass("topLink router-link").attr('url','/members');
   $("#title_1_1 .inner").text("About").addClass("title");
 
   text1 = "The Lunar Gala: an intergalactic celebration of the yearly contributions of the Interplanetary Federation for Peace and Protection. The year is 2268, and, on this 250th anniversary of the planet Earth’s quarantine, we present “<span class='italic'>Ferox</span>”, a public demonstration of the hostility of the Earth’s previously dominant species, the homosapien. We invite Federation sponsors and visitors to observe, for the first time, the savagery we have successfully contained. Infused with Federation principles, the previously dangerous human race is now docile - an allegory for the potential of the IFPP to mollify threatening agents intergalactically. Come celebrate with us! And, in the process, bear witness to a species, that, prior to this event, has been contained for centuries."
--- public/js/people.js
+++ public/js/people.js
@@ -15,9 +15,9 @@ function populatePeopleHeader () {
 	animateBlock("#title_0_0",0,1);
     animateBlock("#title_1_1",0,1);
     animateBlock("#title_0_6",0,1);
-    $("#title_0_0 .inner").text("About").addClass("topLink");
+    $("#title_0_0 .inner").text("About").addClass("topLink router-link").attr('url','/about');
     $("#title_0_0 .inner").append("<img class='arrow-left' src='./../images/Arrows/pointingleft.png'/>")
-    $("#title_0_6 .inner").text("Lines").addClass("topLink");
+    $("#title_0_6 .inner").text("Lines").addClass("topLink router-link").attr('url','/lines');
     $("#title_0_6 .inner").append("<img class='arrow-right' src='./../images/Arrows/pointingright.png'/>")
     $("#title_1_1 .inner").text("Members").addClass("title");
 }
@@ -104,7 +104,7 @@ function populatePeopleContent () {
 		        });
 
 				index = getIndex(i, j);
-				if (index < board1.length || 
+				if (index < board1.length ||
 					(index >= 60 && index < 60 + designers.length) ||
 					(index >= 90 && index < 90 + models.length) ||
 					(index >= 150 && index < 150 + dancers.length) ) {
@@ -291,7 +291,7 @@ function populatePeopleContent_mobile () {
 		        });
 
 				index = getIndex_mobile(i, j);
-				if (index < board1.length || 
+				if (index < board1.length ||
 					(index >= 45 && index < 45 + designers.length) ||
 					(index >= 75 && index < 75 + models.length) ||
 					(index >= 135 && index < 135 + dancers.length) ) {

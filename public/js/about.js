c5a9c0c838233be74f4076a49bc42b1fb3a5f0ff
Deniz A. Sokullu <deniz.sokullu@ibm.com>
2018-02-01 19:11:02 -0500

    fixed sizing of text in about mobile

--- public/js/about.js
+++ public/js/about.js
@@ -65,7 +65,8 @@ function init_about_mobile(){
     animateBlock('#title_1_0',0,1);
     $("#title_1_0 .inner")[0].innerHTML = "<div class='center-text-container'><div class='center-text title'>About</div></div>"
     animateBlock('#4_1',1,0);
-    $("#4_1 .inner")[0].innerHTML = "<div class='center-text-container'><img class=\"arrow arrow-down\" src=\"images/Arrows/pointingdown.png\"><div class='center-text title' style='font-size:9pt; position:relative;top:7vh'>Experience<br><br>Ferox</div></div>"
+    $("#4_1 .inner")[0].innerHTML = "<div class='center-text-container'><img class=\"arrow arrow-down\" src=\"images/Arrows/pointingdown.png\"><div class='center-text title' style='font-size:9pt;position:relative;top:7vh'>Experience<br><br>Ferox</div></div>"
+
     $("#4_1 .inner").off('tap').on('tap',function(){
       movePage(currentPage,grid_rows/row_per_page,'down',function(newPage){
         currentPage = newPage;
@@ -73,7 +74,14 @@ function init_about_mobile(){
       });
     })
     animateBlock('#1_0',2,2);
-      $(".mainGrid #1_0 .inner")[0].innerHTML = "<div id='videoAboutContainer'><video id='videoAbout' width=\"320\" height=\"240\" autoplay=\"autoplay\" loop><source src=\"video/blue_heart.mp4\" type=\"video/mp4\" ></video><div class='mobileAboutText1'>The Lunar Gala: an intergalactic celebration of the yearly contributions of the Interplanetary Federation for Peace and Protection. The year is 2268, and, on this 250th anniversary of the planet Earth’s quarantine, we present “<span class='italic'>Ferox</span>”, a public demonstration of the hostility of the Earth’s previously dominant species, the homosapien. We invite Federation sponsors and visitors to observe, for the first time, the savagery we have successfully contained. Infused with Federation principles, the previously dangerous human race is now docile - an allegory for the potential of the IFPP to mollify threatening agents intergalactically. Come celebrate with us! And, in the process, bear witness to a species, that, prior to this event, has been contained for centuries.</div></div>";
+      $(".mainGrid #1_0 .inner")[0].innerHTML = "<div id='videoAboutContainer'><video id='videoAbout' width=\"320\" height=\"240\" autoplay=\"autoplay\" loop><source src=\"video/blue_heart.mp4\" type=\"video/mp4\" ></video><div class='mobileText1Parent'><div class='mobileAboutText1'>The Lunar Gala: an intergalactic celebration of the yearly contributions of the Interplanetary Federation for Peace and Protection. The year is 2268, and, on this 250th anniversary of the planet Earth’s quarantine, we present “<span class='italic'>Ferox</span>”, a public demonstration of the hostility of the Earth’s previously dominant species, the homosapien. We invite Federation sponsors and visitors to observe, for the first time, the savagery we have successfully contained. Infused with Federation principles, the previously dangerous human race is now docile - an allegory for the potential of the IFPP to mollify threatening agents intergalactically. Come celebrate with us! And, in the process, bear witness to a species, that, prior to this event, has been contained for centuries.</div></div></div>";
+
+    if(window.innerWidth < 425){
+      $(".mobileAboutText1").css('font-size','8pt');
+    }
+    if(window.innerWidth < 350){
+      $(".mobileAboutText1").css('font-size','7pt');
+    }
     //place the first paragraph here
 
     //move down botton
@@ -81,7 +89,10 @@ function init_about_mobile(){
     // $("#6_0")
     text2 = "<div>“<span class='italic'>Ferox</span>” is primal. “<span class='italic'>Ferox</span>” is chaotic. “<span class='italic'>Ferox</span>” is a return to instinct. “<span class='italic'>Ferox</span>” is everything that we, as Federation members, are not. Embodied by the human species, “<span class='italic'>Ferox</span>” represents a blind and reckless freedom that must be contained for the sake of maintaining the civility we all value and from which we all benefit. Ultimately… “<span class='italic'>Ferox</span>” must be quelled.<br><span class='aboutLine'></span>Lunar Gala is curated by Federation members at Carnegie Mellon University. A student-run organization with an interdisciplinary approach, the Lunar Gala team - comprised of student designers, models, dancers, videographers, motion designers, and technicians - strives to showcase the creative visions of our community members. Together, we present an IFPP-endorsed evening of imagination, innovation, and fashion.</div>"
     animateBlock("#9_0",2,2);
-    $(".mainGrid #9_0 .inner").css('font-size','9pt');
+    $(".mainGrid #9_0 .inner").css('font-size','8pt');
+    if(window.innerWidth < 350){
+      $(".mainGrid #9_0 .inner").css('font-size','7pt');
+    }
     $(".mainGrid #9_0 .inner").addClass("text");
     $(".mainGrid #9_0 .inner")[0].innerHTML = text2;
 }
